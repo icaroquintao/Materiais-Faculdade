6540c4d707e63b68304b01c3a45c3559b110b0cb
Icaro Quintão <icarobicalho@hotmail.com>
2020-02-26 20:36:42 -0300

    Trabalho 01 Compiladores
    Desenvolvido em 2017

---
+++ Compiladores/Trabalho Gerador de Analisador Lex Icaro Quintao/Constants.java
@@ -0,0 +1,19 @@
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_If = 2;
+    int t_Then = 3;
+    int t_Else = 4;
+    int t_Elseif = 5;
+    int t_While = 6;
+    int t_Tipo = 7;
+    int t_Variável = 8;
+    int t_Operador = 9;
+    int t_Atribuição = 10;
+    int t_Inteiro = 11;
+    int t_Float = 12;
+    int t_Pontuação = 13;
+
+}
